0f9d58468033044075c5eb8224e2bfa1010c910b
gsveerendrapatil-ops <gsveerendrapatil@gmail.com>
2026-04-27 17:19:48 +0530

    final-commit

--- student_course_registration/student_course_registration/settings.py
+++ student_course_registration/student_course_registration/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,13 +79,16 @@ WSGI_APPLICATION = "student_course_registration.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DB_URL') or os.getenv('DB_URL')
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=DATABASE_URL,
         conn_max_age=600,
-        conn_health_checks=True,
+        ssl_require=False,
     )
 }
+
 
 
 # Password validation
